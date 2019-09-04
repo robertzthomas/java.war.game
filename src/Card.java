645399e18b5cd5b2c505154fde593550bbbdffbb
robertzthomas <zacthomas86@gmail.com>
2019-09-03 22:20:49 -0700

    first commit

---
+++ src/Card.java
@@ -0,0 +1,12 @@
+public class Card {
+private int value;
+private String name;
+//the constructor for the card class
+public Card (int value , String name)
+{
+this.value = value;
+this.name = name;
+}
+
+}
+
